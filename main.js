d18972ba1da6d94daaf4fc62199b786ffa545b88
barroso2223 <barroso8292@gmail.com>
2024-12-17 09:34:30 -0500

    Completed Challenge 2

--- main.js
+++ main.js
@@ -27,3 +27,27 @@ function solution(string, substring) {
 */
 
 /* Challenge 2
+
+Convert number to reversed array of digits
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example(Input => Output):
+35231 => [1,3,2,5,3]
+0 => [0]
+
+
+Solution... */
+
+function digitize(n) {
+    return String(n)//Converts number into a string
+        .split('')//Splits the string into individual characters
+        .reverse()//reverse the order of the characters
+        .map(Number);//Converts the characters back into a number
+}
+
+or 
+
+const digitize = n => String(n)//Converts the numbers into a string
+    .split('')//Splits the string into individual characters
+    .reverse()//Reverses the order of the characters
+    .map(Number);//converts each character back into a number
